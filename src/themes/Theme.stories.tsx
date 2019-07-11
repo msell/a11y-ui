99da0611b9447afbdf5f21695d94af90b1bb2403
Matt <mwsell@gmail.com>
2019-07-11 11:48:53 -0500

    display palettes

--- src/themes/Theme.stories.tsx
+++ src/themes/Theme.stories.tsx
@@ -1,5 +1,16 @@
 import * as React from "react";
 import { storiesOf } from "@storybook/react";
 import { Palette } from "./styleguide/Palette";
-
-storiesOf("Themes", module).add("default", () => <Palette />);
+import { eggplant, black } from "./";
+storiesOf("Themes", module).add("palettes", () => (
+  <div
+    style={{
+      display: "flex",
+      marginBottom: "10px",
+      justifyContent: "space-around",
+    }}
+  >
+    <Palette theme={eggplant} />
+    <Palette theme={black} />
+  </div>
+));
--- src/themes/styleguide/Palette.tsx
+++ src/themes/styleguide/Palette.tsx
@@ -5,29 +5,43 @@ import { withTheme } from "emotion-theming";
 const Container = styled.div`
   display: flex;
   flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-around;
   padding: 20px;
   border: solid gainsboro 2px;
+  margin: 5px;
 `;
 
 const Row = styled.div`
   margin: 5px;
 `;
 
+const Header = styled.div`
+  text-align: center;
+  width: 100%;
+  background-color: #808080;
+  color: #fff;
+`;
 export const Palette = withTheme((props: any) => {
   const { palette } = props.theme;
 
   return (
     <Container>
+      <Header>
+        <h1>{props.theme.name}</h1>
+      </Header>
       {Object.entries(palette).map(([key, value]: any) => (
         <Row key={key}>
-          <div>{key}</div>
+          <div style={{ textAlign: "center" }}>{key}</div>
 
           <div
             style={{
               width: "100px",
               height: "100px",
               borderRadius: "10px",
+              marginTop: "3px",
               backgroundColor: value,
+              border: "1px solid gainsboro",
             }}
           />
         </Row>
